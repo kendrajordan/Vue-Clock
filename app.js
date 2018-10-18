05ac2dc38be245bde0f6a51580f33ccd7b5ee224
Kendra Jordan <kendra.jordan8@gmail.com>
2018-10-17 20:59:49 -0400

    Alarm set

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ let app = new Vue({
 
     data: {
 
-        clockstuff: '00:00:00'
+        clockstuff: '00:00:00',
+        alarmstuff: new Date(),
+        hours:localStorage.getItem('save_hours'),
+        minutes:localStorage.getItem('save_minutes'),
+        seconds:localStorage.getItem('save_seconds'),
+        isSet:true
+
 
     },
 
@@ -17,7 +23,8 @@ let app = new Vue({
             self.clockstuff = self.setTime();
         }, 1000);
 
-    },
+},
+
 
     methods: {
 
@@ -41,6 +48,46 @@ let app = new Vue({
             }
 
             return hours + ':' + minutes + ':' + seconds;
+
+        },
+        setAlarm:function(){
+          //let alarmItems = [];
+          localStorage.setItem('save_hours',this.hours);
+          localStorage.setItem('save_minutes',this.minutes);
+          localStorage.setItem('save_seconds',this.seconds);
+
+          this.alarmstuff.setHours(this.hours,this.minutes,this.seconds);
+           let millialarm = Date.parse(this.alarmstuff);
+
+           let now = new Date();
+           let timer = now.getTime();
+           /*let Hours = now.getHours();
+           if (Hours < 10) {
+               Hours = '0' + Hours;
+           }
+
+           let Minutes = now.getMinutes();
+           if (Minutes < 10) {
+               Minutes = '0' + Minutes;
+           }
+
+           let Seconds = now.getSeconds();
+           if (Seconds < 10) {
+               Seconds = '0' + Seconds;
+           }*/
+           //now.setHours(Hours,Minutes,Seconds);
+           //let millinow = Date.parse(now);
+          // let remainder = millialarm - millinow;
+          let timerremainder = millialarm-timer;
+          if(millialarm > timer){
+           setTimeout(function(){
+              document.getElementById('xyz').play(); document.getElementById('time').innerHTML='Time is Up';},timerremainder);
+              this.isSet=true
+            }
+          else{
+            this.isSet=false;
+
+          }
 
         }
 
